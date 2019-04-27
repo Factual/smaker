84c4f3d85017fa6f6443da58b3d45b0151363d19
Max Hoffman <max@factual.com>
2019-04-27 18:52:36 +0000

    Adds dependencies for pip package

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ setup(
     name='smaker',
     version='0.0.1',
     packages=['snakerunner'],
+    install_requires=[
+        'click',
+        'backports.tempfile',
+        'snakemake',
+        'numpy'
+    ],
     entry_points={
         'console_scripts': ['smaker=snakerunner.cli:main']
     }
