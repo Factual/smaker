cd4034e6573bded354467ad9e23c87cb0af3df4a
Max Hoffman <max@factual.com>
2019-09-24 19:06:04 +0000

    Alternate flag handling

--- examples/simple/transform.py
+++ examples/simple/transform.py
@@ -6,12 +6,12 @@ import pandas as pd
 @click.option('--input-path', required=True)
 @click.option('--output-path', required=True)
 @click.option('--multiplier', default=1)
-@click.option('--sqrt', default='False')
+@click.option('--sqrt/--no-sqrt', is_flag=True, default=False)
 def main(input_path, output_path, multiplier, sqrt):
     df = pd.read_parquet(input_path)
     multiplied = df.multiply(multiplier)
 
-    if sqrt == 'True' : out_df = multiplied.pow(.5)
+    if sqrt: out_df = multiplied.pow(.5)
     else: out_df = multiplied
     out_df.to_csv(output_path, sep='\t')
 
