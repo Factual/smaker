6c820539266bf672b2486660e365d7e8714ab43d
aileen <aileen@factual.com>
2019-09-24 10:40:43 -0700

    fix sqrt flag bug in simple workflow

--- examples/simple/transform.py
+++ examples/simple/transform.py
@@ -6,16 +6,16 @@ import pandas as pd
 @click.option('--input-path', required=True)
 @click.option('--output-path', required=True)
 @click.option('--multiplier', default=1)
-@click.option('--sqrt/--no-sqrt', is_flag=True, default=False)
+@click.option('--sqrt', default=False)
 def main(input_path, output_path, multiplier, sqrt):
     df = pd.read_parquet(input_path)
     multiplied = df.multiply(multiplier)
 
     if sqrt: out_df = multiplied.pow(.5)
     else: out_df = multiplied
-
     out_df.to_csv(output_path, sep='\t')
 
 if __name__=='__main__':
+
     main()
 
